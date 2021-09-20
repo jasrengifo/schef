d14604a3e8a72596ec3a771add6a624ca8916fe3
Jhon Sanchez <jhon.sanchez@trigenius.pt>
2021-09-20 19:11:13 +0100

    Primeir commit

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,17 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
+import $ from 'jquery';
 
 // start the Stimulus application
 import './bootstrap';
+
+$(document).ready(
+    function(){
+        $('[data-toggle="popover"]').popover();
+    }
+);
+
+console.log('test');
